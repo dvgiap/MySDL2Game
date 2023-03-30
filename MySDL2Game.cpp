a8cfc12e2acfbe1aa5275ded9cd8e6bc677c8876
dvgiap <juuzou152@gmail.com>
2023-03-30 21:50:31 +0700

    add lib

--- MySDL2Game.cpp
+++
@@ -1,8 +0,0 @@
-﻿// file cũ bị lỗi không push lên được git hub nên mình tạo project mới
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+
+
+int main(int argc, char* argv[])
+{
+    
+    return 0;
+}
